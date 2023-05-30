006f30a8cbc4282b4bfcc81798eeb6fbe39e6472
Johnny Wordsworth <jwordsw@gmail.com>
2023-05-30 15:44:53 +0100

    updated profiles/show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+
+puts "Destorying all users"
+User.destroy_all
+puts "Destorying all profiles"
+Profile.destroy_all
+
 category = %w[frontend backend fullstack]
 
 
