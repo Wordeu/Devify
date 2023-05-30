2c04ccb9e48da5313eff4ccf3d75b8177e176c21
Johnny Wordsworth <jwordsw@gmail.com>
2023-05-30 14:37:58 +0100

    validates

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :profile
+  validates  start_time: :end_time, presence: true
+  validates :required, inclusion: [true, false]
 end
